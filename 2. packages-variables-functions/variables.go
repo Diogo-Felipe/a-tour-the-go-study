b384290c8915495717e0b15b991414e3ed2a957f
Diogo-Felipe <dlcfelipp@gmail.com>
2023-12-26 08:13:43 -0300

    variables

---
+++ 2. packages-variables-functions/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+var i, j int = 1, 2
+
+func main() {
+	var k int
+	fmt.Println(k, c, python, java)
+	fmt.Println(i, j)
+
+	l := 3
+
+	fmt.Println(l)
+}
